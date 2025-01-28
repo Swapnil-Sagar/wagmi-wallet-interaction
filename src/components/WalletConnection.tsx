c1904946c8eeb747f66cd4c2d3c69b5d0df80a4b
Swapnil Sagar <swapnil.swapnil.sagar946@gmail.com>
2025-01-29 02:44:26 +0530

    Wallet details ui

--- src/components/WalletConnection.tsx
+++ src/components/WalletConnection.tsx
@@ -9,9 +9,19 @@ import {
   useSignMessage
 } from 'wagmi'
 import { mainnet, bsc } from 'wagmi/chains'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
 import { Button } from '@/components/ui/button'
 import { Card, CardContent } from '@/components/ui/card'
-import { Loader2 } from 'lucide-react'
+import { Loader2, LogOut, ChevronDown } from 'lucide-react'
+import { shortenAddr } from '@/lib/utils'
+import Synergy from '../assets/images/Synergy.svg'
+import Eth from '../assets/images/Ethereum.svg'
+import Bsc from '../assets/images/Bsc.svg'
 
 interface WalletConnectionProps {
   onError: (error: string) => void
@@ -109,67 +119,97 @@ export function WalletConnection({ onError }: WalletConnectionProps) {
   }
 
   return (
-    <Card>
-      <CardContent className='space-y-4 mt-4'>
-        <div>
-          <p className='text-sm font-medium text-gray-500'>Connected Address</p>
-          <p className='text-sm font-mono break-all'>{address}</p>
+    <Card className='bg-gray-50 p-6 max-w-xl space-y-4'>
+      <CardContent className='space-x-4 mt-4 flex items-center justify-between'>
+        <div className='flex items-center px-2 rounded-md justify-between w-fit gap-2 bg-accent-foreground'>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant='outline' className='flex items-center bg-transparent'>
+                {isSwitchingNetwork ? (
+                  <Loader2 className='h-4 w-4 animate-spin' />
+                ) : (
+                  <img src={chainId === mainnet.id ? Eth : Bsc} alt='icon' className='h-5 w-5' />
+                )}
+                <span>{chainId === mainnet.id ? 'Ethereum' : 'BSC'}</span>
+                <ChevronDown className='h-4 w-4' />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem>
+                {' '}
+                <Button
+                  className='w-full justify-start'
+                  variant={chainId === mainnet.id ? 'default' : 'outline'}
+                  onClick={() => handleSwitchNetwork(mainnet.id)}
+                  disabled={isSwitchingNetwork}
+                  size='sm'
+                >
+                  <img src={Eth} alt='icon' className='h-5 w-5' />
+                  Ethereum
+                </Button>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Button
+                  className='w-full justify-start'
+                  variant={chainId === bsc.id ? 'default' : 'outline'}
+                  onClick={() => handleSwitchNetwork(bsc.id)}
+                  disabled={isSwitchingNetwork}
+                  size='sm'
+                >
+                  <img src={Bsc} alt='icon' className='h-5 w-5' />
+                  BSC
+                </Button>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
-
-        <div>
-          <p className='text-sm font-medium text-gray-500'>Current Balance</p>
-          {isBalanceLoading ? (
-            <Loader2 className='h-4 w-4 animate-spin' />
-          ) : (
-            <p className='text-sm'>
-              {balance?.formatted} {balance?.symbol}
-            </p>
-          )}
-        </div>
-
-        <div>
-          <p className='text-sm font-medium text-gray-500 mb-2'>Network Selection</p>
-          <div className='flex space-x-2'>
-            <Button
-              variant={chainId === mainnet.id ? 'default' : 'outline'}
-              onClick={() => handleSwitchNetwork(mainnet.id)}
-              disabled={isSwitchingNetwork}
-              size='sm'
-            >
-              Ethereum
-            </Button>
-            <Button
-              variant={chainId === bsc.id ? 'default' : 'outline'}
-              onClick={() => handleSwitchNetwork(bsc.id)}
-              disabled={isSwitchingNetwork}
-              size='sm'
-            >
-              BSC
-            </Button>
-          </div>
-        </div>
-
-        <div className='space-y-2'>
-          <Button
-            onClick={handleSignMessage}
-            disabled={isSigningMessage || hasSignedMessage}
-            className='w-full'
-          >
-            {isSigningMessage ? (
-              <>
-                <Loader2 className='mr-2 h-4 w-4 animate-spin' />
-                Signing...
-              </>
-            ) : hasSignedMessage ? (
-              'Message Signed'
+        <div className='flex items-center px-2 rounded-md justify-between w-fit gap-2 bg-accent-foreground'>
+          <div>
+            {isBalanceLoading || isSwitchingNetwork ? (
+              <Loader2 className='h-4 w-4 animate-spin' />
             ) : (
-              'Sign Message'
+              <p className='text-sm font-semibold'>
+                {balance?.formatted} {balance?.symbol}
+              </p>
             )}
-          </Button>
-
-          <Button onClick={handleDisconnect} variant='outline' className='w-full'>
-            Disconnect
-          </Button>
+          </div>
+          <span className='text-[#E1E4EA]'>|</span>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant='ghost' className='flex items-center bg-accent-foreground'>
+                <img src={Synergy} alt='icon' className='h-5 w-5' />
+                <span>{shortenAddr(address || '', 4, 4)}</span>
+                <ChevronDown className='h-4 w-4' />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem>
+                <Button
+                  onClick={handleSignMessage}
+                  disabled={isSigningMessage || hasSignedMessage}
+                  className='w-full'
+                >
+                  {isSigningMessage || isSwitchingNetwork ? (
+                    <>
+                      <Loader2 className='mr-2 h-4 w-4 animate-spin' />
+                      Signing...
+                    </>
+                  ) : hasSignedMessage ? (
+                    'Message Signed'
+                  ) : (
+                    'Sign Message'
+                  )}
+                </Button>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                {' '}
+                <Button onClick={handleDisconnect} variant='outline' className='w-full'>
+                  <LogOut className='h-5 w-5' />
+                  Disconnect
+                </Button>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       </CardContent>
     </Card>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,13 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export const shortenAddr = (text: string, first = 6, last = 4) => {
+  if (text.length <= first + last) {
+    return text
+  }
+  return text.slice(0, first) + '...' + text.slice(text.length - last)
 }
