f64f9e683c49d088e367490176b5a5b980c616e8
Swapnil Sagar <swapnil.swapnil.sagar946@gmail.com>
2025-02-04 01:05:10 +0530

    Footer tooltip

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,25 @@
 import { Github } from 'lucide-react'
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip'
 
 const Footer = () => {
   return (
-    <div title='By Swapnil Sagar' className='absolute bottom-0 right-0 m-4 hover:animate-bounce'>
-      <a
-        href='https://github.com/Swapnil-Sagar/wagmi-wallet-interaction'
-        target='_blank'
-        rel='noreferrer'
-      >
-        <Github className='h-6 w-6' />
-      </a>
+    <div className='absolute bottom-0 right-0 p-4 '>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <a
+              href='https://github.com/Swapnil-Sagar/wagmi-wallet-interaction'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <Github className='h-7 w-7 hover:animate-bounce' />
+            </a>
+          </TooltipTrigger>
+          <TooltipContent side='left'>
+            <p>By Swapnil Sagar</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
     </div>
   )
 }
